326389185f1398aa25f4af03d34fb204c54a06be
Matthew Marcus <mmarcus@viprsolutions.com>
2018-09-18 16:44:51 +0100

    F# SpaceAge suggested improvements

--- fsharp/space-age/SpaceAge.fs
+++ fsharp/space-age/SpaceAge.fs
@@ -3,15 +3,15 @@
 type Planet = Earth | Mercury | Venus | Mars | Jupiter | Saturn | Uranus | Neptune
 
 let age (planet: Planet) (seconds: int64): float =
-  let years = float(seconds) / (365.25 * 24.0 * 60.0 * 60.0)
+  let years = float seconds / (365.25 * 24.0 * 60.0 * 60.0)
   let multiplier =
     match planet with
     | Earth   -> 1.0
     | Mercury -> 0.2408467
-    | Venus -> 0.61519726
-    | Mars -> 1.8808158
+    | Venus   -> 0.61519726
+    | Mars    -> 1.8808158
     | Jupiter -> 11.862615
-    | Saturn -> 29.447498
-    | Uranus -> 84.016846
+    | Saturn  -> 29.447498
+    | Uranus  -> 84.016846
     | Neptune -> 164.79132
   years / multiplier
